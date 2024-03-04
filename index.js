c95e7b2b72e4796a6a1293d37f8d5b2710c9e475
syahdan firmansyah <syahdan22001@mail.unpad.ac.id>
2024-03-04 09:40:02 +0700

    Mengubah WEB

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('index');
 })
 
 app.listen(port, () => {
